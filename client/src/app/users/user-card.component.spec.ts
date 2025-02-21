8ffda8f2ccb3397a5255b228bce53d8fc7a78dbc
K.K. Lamberty <lamberty@morris.umn.edu>
2025-02-20 23:58:37 -0600

    Remove unsupported call to input in user-card test

--- client/src/app/users/user-card.component.spec.ts
+++ client/src/app/users/user-card.component.spec.ts
@@ -1,12 +1,13 @@
-import { input } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserCardComponent } from './user-card.component';
+import { User } from './user';
 
 describe('UserCardComponent', () => {
   let component: UserCardComponent;
   let fixture: ComponentFixture<UserCardComponent>;
+  let expectedUser: User;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -23,7 +24,7 @@ describe('UserCardComponent', () => {
     fixture = TestBed.createComponent(UserCardComponent);
     component = fixture.componentInstance;
     TestBed.runInInjectionContext(() => {
-      component.user = input({
+      expectedUser = {
         _id: 'chris_id',
         name: 'Chris',
         age: 25,
@@ -31,12 +32,21 @@ describe('UserCardComponent', () => {
         email: 'chris@this.that',
         role: 'admin',
         avatar: 'https://gravatar.com/avatar/8c9616d6cc5de638ea6920fb5d65fc6c?d=identicon'
-      })
+      };
     });
+    fixture.componentRef.setInput('user', expectedUser);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be associated with the correct user', () => {
+    expect(component.user()).toEqual(expectedUser);
+  });
+
+  it('should be the user named Chris', () => {
+    expect(component.user().name).toEqual('Chris');
   });
 });
